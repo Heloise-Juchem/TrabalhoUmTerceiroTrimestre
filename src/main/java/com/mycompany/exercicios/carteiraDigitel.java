fe4c56f610b9697bde964a32588b861b7fe0a77d
heloise-juchem <heloise.juchem@escola.pr.gov.br>
2026-09-17 11:26:48 -0300

    Primeiro commit

---
+++ src/main/java/com/mycompany/exercicios/carteiraDigitel.java
@@ -0,0 +1,13 @@
+package com.mycompany.exercicios;
+
+public class carteiraDigitel {
+    public String titular;
+    public Float saldo;
+    public Double num_transacoes;
+    
+    public carteiraDigitel(String titular, Float saldo, Double num_transacoes){
+        this.titular = titular;
+        this.saldo = saldo;
+        this.num_transacoes = num_transacoes;
+    }
+}
---
+++ src/main/java/com/mycompany/trabalhoumterceirotrimestre/TrabalhoUmTerceiroTrimestre.java
@@ -0,0 +1,43 @@
+package com.mycompany.trabalhoumterceirotrimestre;
+//import com.mycompany.exercicios.Pessoa;
+//import com.mycompany.exercicios.Produto;
+//import com.mycompany.exercicios.Livros;
+//import com.mycompany.exercicios.Veiculo;
+
+public class TrabalhoUmTerceiroTrimestre {
+
+    public static void main(String[] args) {
+        //Pessoa lucas = new Pessoa("Lucas de Souza", 24, (float)1.74);
+        
+        //System.out.printf("%s", lucas.exibirDados());
+        //System.out.printf("%s", lucas.fazerAniversario());
+        //System.out.printf("%s", lucas.exibirDados());
+        
+        //Produto maca = new Produto("Maça", (float) 1.50, 300);
+        
+        //System.out.printf("%s", maca.exibirResumo());
+        //System.out.printf("%s", maca.adicionarEstoque(20));
+        //System.out.printf("%s", maca.adicionarEstoque(30));
+        //System.out.printf("%s", maca.adicionarEstoque(100));
+        //System.out.printf("%s", maca.vender(8));
+        //System.out.printf("%s", maca.vender(10));
+        //System.out.printf("%s", maca.vender(6));
+        
+        //Livros rainha_vermelha = new Livros ("Rainha Vermelha", "Victoria Aveyard", 424, (float) 70);
+        
+        //System.out.printf("%s", rainha_vermelha.exibirDetalhes());
+        //System.out.printf("%s", rainha_vermelha.aplicarDesconto(-20.00));
+        
+        //Veiculo fusca = new Veiculo("Volkswagen", "fusca", 1979, (double) 300000);
+        //Veiculo kombi = new Veiculo("Volkswagen", "kombi", 1950, (double) 500000);
+        
+        
+        //System.out.printf("%s", fusca.calcularTempo());
+        //System.out.printf("%s", fusca.registrarViagem((double) 500));
+        //System.out.printf("%S", fusca.exibirDados());
+        //System.out.printf("%s", kombi.calcularTempo());
+        //System.out.printf("%s", kombi.registrarViagem((double) 500));
+        //System.out.printf("%S", kombi.exibirDados());
+
+    }
+}
